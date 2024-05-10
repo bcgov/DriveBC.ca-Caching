e746007b7714a1e7e6ea94ab58ff7c5131161fd7
Mulder <Willem.Mulder@gov.bc.ca>
2024-05-10 14:01:41 -0700

    DBC22-2113: Update logging process

---
+++ compose/caching/openshiftjobs/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Check the number of arguments
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <script_name> [args...]"
+    exit 1
+fi
+
+# Determine which script to run based on the first argument
+case "$1" in
+    ziplogs)
+        # Run ziplogs.sh which will zip all files that were created in the previous hour or older in the nginx log storage pvc
+        /scripts/ziplogs.sh
+        ;;
+    analyzeexportlogs)
+        # Run analyzeexportlogs with additional arguments which will send the specified days logs through goaccess and then upload to s3.
+        shift # Remove the first argument (script number)
+        /scripts/analyzeexportlogs.sh "$@"
+        ;;
+    *)
+        echo "Invalid script"
+        exit 1
+        ;;
+esac
